2ada95437cd91aecf60089fd14a4cc875b4cc3f3
Shivam-1409 <shivampuri1409@gmail.com>
2026-02-12 21:43:40 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from post_generator import get_length_str
+from post_generator import generate_post
+from few_shot import FewShotPosts
+
+def main():
+    st.title("Linkedin Post Generator")
+    col1,col2,col3 = st.columns(3)
+    fs = FewShotPosts()
+    with col1:
+        selected_tag = st.selectbox("Title",options = fs.get_tags())
+    with col2:
+        selected_length = st.selectbox("Length",options=["Short","Medium","Long"])
+    with col3:
+        selected_language = st.selectbox("Language",options=["English","Hinglish"])
+
+    if st.button("Generate"):
+        selected_length = get_length_str(selected_length)
+        post_generated = generate_post(selected_length,selected_language,selected_tag)
+        st.subheader("Generated Post")
+        st.text(post_generated)
+
+
+if __name__ == "__main__":
+    main()
